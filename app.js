afef74a26d1347dbf0e985cb99af0173ceaebc47
joeyborai <joeyborai1@gmail.com>
2021-03-21 17:05:18 -0400

    push changes to git.

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Adding in routes
 app.use('/catalog', catalogRouter);
 app.use('/orders', orderRouter);
 
